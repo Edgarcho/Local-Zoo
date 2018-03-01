63c901e76c034f3a18cbc06a29bd4491c4f5921c
Edgar Hernandez <edgarchortiz@gmail.com>
2018-03-01 10:44:41 -0800

    Create animal model and refactor animal-list

---
+++ app/animal-list.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Animal } from './animal-modele';
+
+@Component({
+  selector: 'animal-list',
+  template: `
+  <ul>
+    <li *ngFor='let currentAnimal of animals'>{{currentAnimal.species}} <button (click)='editAnimal(currentAnimal)'>Edit!</button></li>
+  </ul>
+  `
+})
+
+export class AnimalListComponent {
+  animals: Animal[] = [
+    new Animal('Arctic Fox','Moon', 2, 'Carnivore', 'Northern Trail', 5, 'Female', 'Cool shade', 'Loud noises'),
+    new Animal('Ocelot','Prince', 4, 'Carnivore', 'Tropical Rain Forest Building' , 6, 'Male', 'Laying in the sunshine', 'Toys that are not rope-based'),
+    new Animal('Northwest Black Tailed Deer', 'Tinkerbell', 8, 'Herbivore', 'Northern Trail', 2, 'Female', 'Delicate roots and leaves', 'Loud Noises')
+    ];
+
+}
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Animal } from './animal-modele';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +7,8 @@ import { Component } from '@angular/core';
   <div class='container'>
     <h1>Portland Zoo</h1>
     <p>List of animal at the zoo<p>
-    <ul>
-      <li *ngFor='let currentAnimal of animals'>{{currentAnimal.species}} <button (click)='editAnimal(currentAnimal)'>Edit!</button></li>
-    </ul>
+    <animal-list></animal-list>
+
     <div>
       <div *ngIf='selectedAnimal'>
         <h3>{{selectedAnimal.species}}</h3>
@@ -34,11 +34,7 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-animals: Animal[] = [
-  new Animal('Arctic Fox','Moon', 2, 'Carnivore', 'Northern Trail', 5, 'Female', 'Cool shade', 'Loud noises'),
-  new Animal('Ocelot','Prince', 4, 'Carnivore', 'Tropical Rain Forest Building' , 6, 'Male', 'Laying in the sunshine', 'Toys that are not rope-based'),
-  new Animal('Northwest Black Tailed Deer', 'Tinkerbell', 8, 'Herbivore', 'Northern Trail', 2, 'Female', 'Delicate roots and leaves', 'Loud Noises')
-  ];
+
   selectedAnimal: null;
 
   editAnimal(clickedAnimal) {
