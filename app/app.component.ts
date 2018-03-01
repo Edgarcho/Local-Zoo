ea35d438fd53fbb67f35eb13769a8322018cba29
Edgar Hernandez <edgarchortiz@gmail.com>
2018-03-01 11:17:26 -0800

    Refactor app and edit animal component

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,28 +7,8 @@ import { Animal } from './animal-modele';
   <div class='container'>
     <h1>Portland Zoo</h1>
     <p>List of animal at the zoo<p>
-    <animal-list [childAnimalsList]"masterAnimalList" (clickSender)='editAnimal($event)'></animal-list>
-
-    <div>
-      <div *ngIf='selectedAnimal'>
-        <h3>{{selectedAnimal.species}}</h3>
-        <h3>Edit Animal</h3>
-        <h4>Name:{{selectedAnimal.name}}</h4>
-        <h4>Caretakers:{{selectedAnimal.caretakers}}</h4>
-        <h4>Age:{{selectedAnimal.age}}</h4>
-        <h4>Sex:{{selectedAnimal.sex}}</h4>
-        <label>Enter name:</label>
-        <input [(ngModel)]='selectedAnimal.name'>
-        <label>Enter caretakers:</label>
-        <input [(ngModel)]='selectedAnimal.caretakers'>
-        <label>Enter Age:</label>
-        <input [(ngModel)]='selectedAnimal.age'>
-        <br>
-        <input type='radio' [(ngModel)]='selectedAnimal.sex' [value]="Male">Male<br>
-        <input type='radio' [(ngModel)]='selectedAnimal.sex' [value]="Female">Female<br>
-        <button (click)='finishedEditing()'>Done</button>
-      </div>
-    </div>
+    <animal-list [childAnimalsList]="masterAnimalList" (clickSender)='editAnimal($event)'></animal-list>
+    <edit-animal [childSelectedAnimal]="selectedAnimal" (doneButtonClickSender)='finishedEditing()'></edit-animal>
   </div>
   `
 })
